b8bbaf39a643c7a095186f927516a9c10557afdf
Elizon Rodrigues <118481749+elizonRL@users.noreply.github.com>
2023-11-27 23:07:21 -0400

    Update .gitignore and app.js

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
+app.get("/", async (req, res) => {
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the beginning of nothingness.');
+  const response = await axios.get(
+    'https://api.github.com/users/elizonRL/repos'
+  );
+  if(response.data.name !== 'api-porfolio' || response.data.name !== 'portfolio'){
+    const repos = response.data;
+    res.send(repos);
+  }
+  
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
